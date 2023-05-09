e49deaeae57310b088ab694e9679c73b3ee5bfa8
Rafaela Gomes <99452621+gsrafaela@users.noreply.github.com>
2023-05-09 11:49:02 -0300

    Add files via upload

---
+++ 05-Jpa-Padroes/src/main/java/br/com/fiap/jpa/dao/InvestimentoDaoImpl.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jpa.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.fiap.jpa.entity.Investimento;
+
+public class InvestimentoDaoImpl extends GenericDaoImpl<Investimento, Integer> implements InvestimentoDao {
+
+	public InvestimentoDaoImpl(EntityManager em) {
+		super(em);
+	}
+	
+}
